d391e3c7fffa467acfcbb8a642b6fa78aab58de0
Gleb Kushedow <kushedow@gmail.com>
2023-01-25 09:05:44 +0400

    assets updated

--- create_socket.py
+++ create_socket.py
@@ -6,11 +6,11 @@ sio = socketio.Server(cors_allowed_origins="*", origins="*")
 # Сервим статику (хотя пока она нам и не нужна)
 app = socketio.WSGIApp(sio, static_files={
 
+        # Разрешаем открывать главную
         '/': {'content_type': 'text/html', 'filename': 'questro_frontend/dist/index.html'},
 
-        '/assets/index-8806e07e.js': {'content_type': 'text/javascript', 'filename': 'questro_frontend/dist/assets/index-8806e07e.js'},
-
-        '/assets/index-6516db60.css': {'content_type': 'text/javascript', 'filename': 'questro_frontend/dist/assets/index-6516db60.css'},
+        # Разрешаем открывать все из папки ассетов
+        '/assets/': 'questro_frontend/dist/assets/'
 
     },
 )
